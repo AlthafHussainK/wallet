c7e9555b09a8dd17b5099613760c4fd8d42798e8
AlthafHussainK <althafakm@gmail.com>
2021-06-20 16:20:16 +0530

    add in-exp component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Header } from './components/Header'
+import { Balance } from './components/Balance'
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 function App() {
   return (
     <div>
-      Hello!
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
