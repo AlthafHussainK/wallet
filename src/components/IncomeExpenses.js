2d7f05c2760c317dbd0a4c534223035e6a7b170b
AlthafHussainK <althafakm@gmail.com>
2021-06-20 20:14:50 +0530

    fix a typo

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -6,11 +6,10 @@ export const IncomeExpenses = () => {
       <div>
         <h4>Income</h4>
         <p className="money plus">+$0.00</p>
-      <div/>
+      </div>
       <div>
         <h4>Expense</h4>
         <p className="money minus">-$0.00</p>
-      </div>
       </div>
     </div>
   )
