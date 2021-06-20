6150d662fb94226ceb23c19a9eb06af4d9365dcf
AlthafHussainK <althafakm@gmail.com>
2021-06-20 23:01:40 +0530

    createContext

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const AddTransaction = () => {
+  const [text, setText] = useState('')
+  const [amount, setAmount] = useState('')
+
   return (
     <>
       <h3>Add new transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Text</label>
-          <input type="text" id="text" placeholder="Enter text..." />
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} id="text" placeholder="Enter text..." />
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount <br/>
             (negative-expense, positive-income)
           </label>
-          <input type="number" id="amount" placeholder="Enter amount" />
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} id="amount" placeholder="Enter amount" />
         </div>
+        <button className="btn">Add Transaction</button>
       </form>
     </>
   )
