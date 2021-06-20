a41a15c034da7f17eaf8aaccde976055ccd65aa2
AlthafHussainK <althafakm@gmail.com>
2021-06-20 23:12:49 +0530

    update the transaction amount

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 export const Transaction = ({transaction}) => {
+  const sign = transaction.amount < 0 ? '-' : '+'
+
   return (
-    <li className="minus">
-      {transaction.text} <span>$300</span><button className="delete-btn">x</button>
+    <li className={transaction.amount > 0 ? 'plus': 'minus'}>
+      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
     </li>
   )
 }
