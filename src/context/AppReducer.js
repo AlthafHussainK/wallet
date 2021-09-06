abe516a699bfb47148a62790d1109b10b87732d9
AlthafHussainK <althafakm@gmail.com>
2021-09-06 14:40:28 +0530

    change appReducer function

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,4 +1,4 @@
-export default (state, action) => {
+const appReducer = (state, action) => {
   switch(action.type) {
     case 'ADD_TRANSACTION':
       return {
@@ -14,3 +14,5 @@ export default (state, action) => {
       return state
   }
 }
+
+export default appReducer
